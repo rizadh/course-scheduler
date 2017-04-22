12760030c11aa4650d97ab65bfab1e7f6197afad
Rizadh Nizam <rizadh@users.noreply.github.com>
2017-04-22 13:51:41 -0400

    Use UMD bundling format

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,8 @@ import typescript from 'rollup-plugin-typescript'
 export default {
     entry: "src/main.ts",
     dest: "dist/main.js",
-    format: 'cjs',
+    format: 'umd',
+    moduleName: 'course-scheduler',
     plugins: [
         typescript({
             typescript: require('typescript'),
