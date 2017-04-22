9c454e51c49fb6b9116953ac714c51c619dced8b
Rizadh Nizam <rizadh@users.noreply.github.com>
2017-04-22 14:26:33 -0400

    Use expect instead of should assertions

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,9 @@
-import * as chai from 'chai';
-chai.should();
+import {expect} from 'chai';
 
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
-      [1, 2, 3].indexOf(4).should.equal(-1);
+      expect([1, 2, 3].indexOf(4)).to.equal(-1);
     });
   });
 });
