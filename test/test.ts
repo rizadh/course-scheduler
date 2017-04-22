6964351e21558e8255ccd9beabca5d86234e2d9e
Rizadh Nizam <rizadh@users.noreply.github.com>
2017-04-22 14:07:03 -0400

    Add a dummy Mocha test

---
+++ test/test.ts
@@ -0,0 +1,10 @@
+import * as chai from 'chai';
+chai.should();
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      [1, 2, 3].indexOf(4).should.equal(-1);
+    });
+  });
+});
